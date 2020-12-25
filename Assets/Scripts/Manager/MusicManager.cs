009540928936dc7baa5d778b573bb749f665c6de
Mitesh Solanki <miteshsolanki360@gmail.com>
2020-12-25 21:43:17 +0530

    Added Music Player

---
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource musicPlayer;
+    public AudioClip[] audioClip;
+    void Start()
+    {
+        musicPlayer.clip = audioClip[Random.Range(0, audioClip.Length)];
+        musicPlayer.Play();
+        Debug.Log("current track is " + musicPlayer.clip.name);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!musicPlayer.isPlaying)
+        {
+            ChangeMusic();
+        }
+    }
+
+    void ChangeMusic()
+    {
+        musicPlayer.clip = audioClip[Random.Range(0, audioClip.Length)];
+        musicPlayer.Play();
+        Debug.Log("current track is " + musicPlayer.clip.name);
+    }
+}
